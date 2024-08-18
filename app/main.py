c7155f45c99cd7a55ec87a4c409571fa9f520f76
kevinwochan <kevinwochan@gmail.com>
2024-08-19 01:55:39 +1000

    added ticker info, USD account, performance reports, CGT calcs

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 """Trade Tracker"""
 
-import datetime
-
 import pandas as pd
 import streamlit as st
+import yfinance as yf
+from aus import financial_year
 
 st.set_page_config(
     page_title="Trade Tracker",
@@ -21,11 +21,16 @@ NORMALISED_COLUMNS = [
     "consideration",
     "brokerage",
     "source",
+    "currency",
 ]
 
 COLUMUMN_CONFIG = {
     "date": st.column_config.DateColumn(
         "Date",
+    ),
+    "weight_pct": st.column_config.NumberColumn(
+        "Weight (%)",
+        format="%.1f",
     ),
 }
 
@@ -47,7 +52,7 @@ def parse_csv(csv):
     return trades
 
 
-def parse_commsec_csv(csv):
+def parse_commsec_csv(csv, currency="AUD"):
     """Date	Reference	Details	Debit($)	Credit($)	Balance($)."""
     transactions = pd.read_csv(
         csv,
@@ -69,12 +74,13 @@ def parse_commsec_csv(csv):
             new_row[0] = pd.to_datetime(row["Date"], format="%d/%m/%Y")
             new_row[1] = commsec_actions[action]
             new_row[2] = row["Reference"]
-            new_row[3] = code
+            new_row[3] = code + ".AX"
             new_row[4] = int(units)
             new_row[5] = float(avg_price)
             new_row[6] = float(consideration)
             new_row[7] = abs(float(consideration) - (float(units) * float(avg_price)))
             new_row[8] = "Commsec CSV"
+            new_row[9] = currency
         except:
             st.error(
                 "Please upload a valid Commsec CSV file from Portfolio -> Accounts -> Transactions -> Set your From and To dates -> Downloads -> CSV",
@@ -88,72 +94,83 @@ def parse_commsec_csv(csv):
     return new_trades
 
 
-def parse_selfwealth_csv(csv):
+def parse_selfwealth_csv(csv, currency="AUD"):
     """Trade Date	Settlement Date	Action	Reference	Code	Name	Units	Average Price	Consideration	Brokerage	Total
     16/08/2024 0:00	20/08/2024 0:00	Buy	1993950	GOLD	GBLX GOLD	141	34.25	4829.25	9.5	4838.75
 
     """
-    try:
-        trades = pd.read_csv(
-            csv,
-            usecols=[
-                "Trade Date",
-                "Action",
-                "Reference",
-                "Code",
-                "Units",
-                "Average Price",
-                "Consideration",
-                "Brokerage",
-            ],
-            converters={
-                "Trade Date": lambda x: pd.to_datetime(x, format="%Y-%m-%d %H:%M:%S"),
-                "Units": int,
-            },
-        )
-        trades["source"] = "Selfwealth CSV"
-
-        trades.columns = NORMALISED_COLUMNS
-        # translate this: 2021-05-28 00:00:00 to a Datetime object
-        return trades
-    except Exception as e:
-        st.error(
-            "Please upload a valid Selfwealth CSV file from Trading Account -> Movements -> Set your time period -> Export X Rows -> As CSV",
-        )
-        print(e)
-    return pd.DataFrame()
+    trades = pd.read_csv(
+        csv,
+        usecols=[
+            "Trade Date",
+            "Action",
+            "Reference",
+            "Code",
+            "Units",
+            "Average Price",
+            "Consideration",
+            "Brokerage",
+        ],
+        converters={
+            "Trade Date": lambda x: pd.to_datetime(x, format="%Y-%m-%d %H:%M:%S"),
+            "Units": int,
+            "Code": lambda x: x + ".AX" if currency == "AUD" else x,
+        },
+    )
+    trades["source"] = "Selfwealth CSV"
+    trades["currency"] = currency
+    trades.columns = NORMALISED_COLUMNS
+    # translate this: 2021-05-28 00:00:00 to a Datetime object
+    return trades
 
 
 # match trades with buy parcels
-def calculate_capital_gains(
-    trades_df,
-    selected_codes=[],
-    year=int(datetime.date.today().year),
-):
-    """Calculate capital gains by taking the highest possible buy parcel for each trade and matching it with the highest possible sell parcel.
-    trades_df could be empty
-    """
+CAPITAL_GAIN_METHODS = [
+    "FIFO",
+    "LIFO",
+    "Minimise Capital Gain Tax",
+]
+
+
+def find_buy_sell_parcels(trades):
+    """Find buy and sell parcels."""
+    buy_parcels = []
+    sell_parcels = []
+    for _, row in trades.iterrows():
+        if row["action"] == "Buy" or row["action"] == "DRP" or row["action"] == "SPP":
+            buy_parcels.append(row)
+        elif row["action"] == "Sell":
+            sell_parcels.append(row)
+
+    return buy_parcels, sell_parcels
+
+
+def apply_cgt_discount(buy_parcels, date):
+    discounted_buy_parcels = []
+    for buy in buy_parcels:
+        buy["contract_price"] = buy["avg_price"]
+        if buy["date"] - date > pd.Timedelta(
+            days=365,
+        ):
+            buy["avg_price"] = buy["avg_price"] / 2
+        discounted_buy_parcels.append(buy)
+
+    discounted_buy_parcels.sort(
+        key=lambda x: x["avg_price"],
+        reverse=True,
+    )
+    return discounted_buy_parcels
+
+
+def calculate_capital_gains(trades_df):
+    """Calculate capital gains by taking the highest possible buy parcel for each trade and matching it with the highest possible sell parcel."""
     if trades_df.empty:
         return trades_df
-    # filter trades by year
-    trades_df = trades_df[trades_df["date"].dt.year == year]
 
-    remaining_holdings = {}
-    for code, sub_df in trades_df.groupby("code"):
-        buy_parcels = []
-        sell_parcels = []
-        for idx, row in sub_df.iterrows():
-            if (
-                row["action"] == "Buy"
-                or row["action"] == "DRP"
-                or row["action"] == "SPP"
-            ):
-                buy_parcels.append(row)
-            elif row["action"] == "Sell":
-                sell_parcels.append(row)
-
-        # match sell parcels with buy parcels
-        capital_gain_events = []
+    capital_gain_events = []
+    # Progressively making these pairs per year ensures the remaining buy parcels can still be used for the next year
+    for code, trades_on_code in trades_df.groupby("code"):
+        [buy_parcels, sell_parcels] = find_buy_sell_parcels(trades_on_code)
 
         # most recent sells first
         sell_parcels.sort(key=lambda x: x["date"], reverse=True)
@@ -162,74 +179,108 @@ def calculate_capital_gains(
 
         for sell_parcel in sell_parcels:
             units_remaining = sell_parcel["units"]
-            for buy_parcel in buy_parcels:
-                # ensure the buy date is before the sell date
-                if (
-                    not buy_parcel["date"] <= sell_parcel["date"]
-                    and buy_parcel["units"] > 0
-                ):
-                    continue
-                # update
-                units_sold = min(units_remaining, buy_parcel["units"])
-                capital_gain_events.append(
-                    {
-                        "code": code,
-                        "buy": {
-                            **buy_parcel,
-                            "units": units_sold,
-                            "value": units_sold * buy_parcel["avg_price"],
-                        },
-                        "sell": {
-                            **sell_parcel,
-                            "value": units_sold * sell_parcel["avg_price"],
-                        },
-                        "capital_gain": units_sold
-                        * (sell_parcel["avg_price"] - buy_parcel["avg_price"]),
-                    },
-                )
-                units_remaining -= units_sold
-                buy_parcel["units"] -= units_sold
-                # all units sold
-                if units_remaining < 1:
-                    break
+            # filter out invalid buy parcels and apply the cgt discount
+            discounted_buy_parcels = apply_cgt_discount(
+                list(
+                    filter(
+                        lambda x: x["date"] <= sell_parcel["date"] and x["units"] > 0,
+                        buy_parcels,
+                    ),
+                ),
+                sell_parcel["date"],
+            )
 
-        # create output table
-        if not capital_gain_events:
+            cgt_event = [sell_parcel]
+            for buy_parcel in discounted_buy_parcels:
+                units_sold = min(units_remaining, buy_parcel["units"])
+                units_remaining -= units_sold
+                buy = buy_parcel.copy(deep=True)
+                buy["units"] = units_sold
+                cgt_event.append(buy)
+                buy_parcel["units"] -= units_sold
+
+                if units_remaining == 0:
+                    break
+            cgt_event.sort(key=lambda x: x["date"])
+            capital_gain_events.append(
+                {
+                    "code": code,
+                    "trades": cgt_event,
+                    "FY": sell_parcel["FY"],
+                },
+            )
+
+    return capital_gain_events
+
+
+def display_capital_gains(
+    trades_df,
+    selected_codes=[],
+):
+    trades_df["FY"] = trades_df["date"].apply(lambda x: financial_year(x))
+    cgt_events = calculate_capital_gains(trades_df)
+    trading_years = sorted(trades_df["FY"].unique().tolist())
+    for year in trading_years:
+        cgt_events_in_year = list(filter(lambda x: x["FY"] == year, cgt_events))
+        if not cgt_events_in_year:
             continue
 
-        st.subheader(code)
-        for event in capital_gain_events:
-            table = pd.DataFrame.from_records(
-                data=[
-                    event["buy"],
-                    event["sell"],
-                    {"capital_gain": event["capital_gain"]},
-                ],
-                columns=[
-                    "date",
+        gain = sum(
+            [
+                sum(
+                    [
+                        -(x["avg_price"] * x["units"])
+                        if x["action"] == "Buy"
+                        else (x["avg_price"] * x["units"])
+                        for x in cgt_event["trades"]
+                    ],
+                )
+                for cgt_event in cgt_events_in_year
+            ],
+        )
+        holdings = find_current_holdings(trades_df[trades_df["FY"] < year])
+        holding_sum = holdings["total_cost"].sum()
+        st.subheader(f"{year}: {gain:,.2f} ({(gain/holding_sum*100):,.2f} %)")
+
+        for cgt_event in cgt_events_in_year:
+            st.dataframe(
+                cgt_event["trades"],
+                column_config={
+                    "date": st.column_config.DateColumn(
+                        "Date",
+                    ),
+                },
+                column_order=[
+                    "code",
                     "action",
-                    "reference",
+                    "date",
                     "units",
                     "avg_price",
-                    "brokerage",
-                    "value",
-                    "capital_gain",
+                    "consideration",
+                    "reference",
+                ],
+                use_container_width=True,
+                hide_index=True,
+            )
+
+            capital_gain = sum(
+                [
+                    -(x["avg_price"] * x["units"])
+                    if x["action"] == "Buy"
+                    else (x["avg_price"] * x["units"])
+                    for x in cgt_event["trades"]
                 ],
             )
-        remaining_buy_parcels = [p for p in buy_parcels if p["units"] > 0]
-        if remaining_buy_parcels:
-            remaining_holdings[code] = remaining_buy_parcels
-    return remaining_holdings
+            st.write(f"Capital Gain: ${capital_gain:,.2f}")
 
 
-def display_current_holdings(corrected_trades):
-    current_holdings = pd.DataFrame(
-        columns=["code", "units", "total_cost", "avg_price"],
-    )
-    for code, sub_df in corrected_trades.groupby("code"):
+def find_current_holdings(trades):
+    current_holdings = pd.DataFrame()
+    for code, sub_df in trades.groupby("code"):
+        sorted_trades = sub_df.sort_values("date", ascending=True)  # FIFO
         units = 0
         total_cost = 0
-        for _, row in sub_df.iterrows():
+        for _, row in sorted_trades.iterrows():
             if (
                 row["action"] == "Buy"
                 or row["action"] == "DRP"
@@ -240,6 +291,7 @@ def display_current_holdings(corrected_trades):
             elif row["action"] == "Sell":
                 total_cost -= row["avg_price"] * row["units"]
                 units -= row["units"]
+
         if units > 0:
             holding_summmary = pd.DataFrame.from_records(
                 data=[
@@ -250,13 +302,61 @@ def display_current_holdings(corrected_trades):
                         "total_cost": total_cost,
                     },
                 ],
-                columns=["code", "units", "avg_price", "total_cost"],
+                columns=[
+                    "code",
+                    "units",
+                    "avg_price",
+                    "total_cost",
+                ],
             )
             current_holdings = pd.concat(
                 [current_holdings, holding_summmary],
                 ignore_index=True,
             )
-    st.dataframe(current_holdings)
+
+    return current_holdings
+
+
+def get_current_value(holdings):
+    holdings["current_price"] = holdings.apply(
+        lambda x: yf.Ticker(x["code"]).info["previousClose"],
+        axis=1,
+    )
+    holdings["current_value"] = holdings["current_price"] * holdings["units"]
+    holdings["profit"] = (
+        holdings["current_price"] * holdings["units"] - holdings["total_cost"]
+    )
+    return holdings
+
+
+def display_current_holdings(corrected_trades):
+    """To find the current holdings, buy/sell parcels are matched with the FIFO method"""
+    current_holdings = pd.DataFrame(
+        columns=["code", "units", "total_cost", "avg_price"],
+    )
+    current_holdings = find_current_holdings(corrected_trades)
+    current_holdings = get_current_value(current_holdings)
+
+    if current_holdings.empty:
+        st.write("No current holdings found")
+        return
+    st.write(
+        f"Portfolio value: ${current_holdings['current_value'].sum():,.2f}",
+    )
+    st.write(f"Total cost: ${current_holdings['total_cost'].sum():,.2f}")
+    st.write(
+        f"Total Profit: ${current_holdings['current_value'].sum() - current_holdings['total_cost'].sum():,.2f}",
+    )
+
+    current_holdings["weight_pct"] = (
+        current_holdings["current_value"] / current_holdings["current_value"].sum()
+    ) * 100
+
+    st.dataframe(
+        current_holdings,
+        use_container_width=True,
+        column_config=COLUMUMN_CONFIG,
+    )
 
 
 def has_sold_more_units_than_bought(sub_df):
@@ -331,14 +431,24 @@ if prev_report:
     movement = parse_csv(prev_report)
     trades = pd.concat([trades, movement], ignore_index=True)
 
-st.subheader("Selfwealth")
+st.subheader("Selfwealth AUD")
 sw_report = st.file_uploader(
     "Trading Account -> Movements -> Set your time period -> Export X Rows -> As CSV",
     type=["csv"],
     key="sw",
 )
 if sw_report:
-    movement = parse_selfwealth_csv(sw_report)
+    movement = parse_selfwealth_csv(sw_report, currency="AUD")
+    trades = pd.concat([trades, movement], ignore_index=True)
+
+st.subheader("Selfwealth USD")
+sw_report = st.file_uploader(
+    "Trading Account -> Movements -> Set your time period -> Export X Rows -> As CSV",
+    type=["csv"],
+    key="sw-usd",
+)
+if sw_report:
+    movement = parse_selfwealth_csv(sw_report, currency="USD")
     trades = pd.concat([trades, movement], ignore_index=True)
 
 st.subheader("Commsec")
@@ -391,6 +501,7 @@ corrected_trades = trades[trades["ignore"] == False]
 # Highlight missing data
 display_invalid_holdings(corrected_trades)
 
+
 st.markdown("---")
 
 
@@ -401,25 +512,7 @@ display_current_holdings(corrected_trades)
 
 
 st.header("FY Capital Gains")
-# select years
-years = [] if trades.empty else trades["date"].dt.year.unique().tolist()
-
-selected_year = st.selectbox(
-    "Select a year to filter by",
-    sorted(years),
-    index=len(years) - 1,
-    key="year",
-)
-
-remaining_holdings = calculate_capital_gains(
+display_capital_gains(
     trades,
     selected_codes=selected_codes,
-    year=selected_year,
-)
-
-calculation_method = st.selectbox(
-    "Select gains calculation method",
-    ["LIFO", "FIFO", "minimise CGT"],
-    index=0,
-    key="calculation_method",
 )
